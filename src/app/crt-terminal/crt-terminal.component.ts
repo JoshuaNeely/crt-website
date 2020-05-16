e1ec31aa061cc459d964efbe8cf3829ddc409732
JoshuaNeely <joshua.a.neely@gmail.com>
2020-05-16 11:00:46 -0500

    polish css. Add blinking cursor

--- src/app/crt-terminal/crt-terminal.component.ts
+++ src/app/crt-terminal/crt-terminal.component.ts
@@ -1,21 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  ViewChild,
+  AfterViewInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-crt-terminal',
   templateUrl: './crt-terminal.component.html',
   styleUrls: ['./crt-terminal.component.scss'],
 })
-export class CrtTerminalComponent implements OnInit {
+export class CrtTerminalComponent implements OnInit, AfterViewInit {
   userInput: string = '';
   terminalLog: string[] = [];
+
+  @ViewChild('terminalInput') testTerm: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    this.testTerm.nativeElement.focus();
+  }
+
   submitLine() {
     this.terminalLog.push(this.userInput);
     this.userInput = '';
+  }
+
+  trapInputFocus() {
+    const element = this.testTerm.nativeElement;
+    // the timeout is very important... uncertain why
+    setTimeout(() => element.focus(), 0);
   }
 }

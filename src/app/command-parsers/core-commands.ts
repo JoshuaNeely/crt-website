053ed584dd6b89c606f35cedd7517de783837c26
JoshuaNeely <joshua.a.neely@gmail.com>
2020-05-16 20:23:14 -0500

    add short descriptions to list-commands

--- src/app/command-parsers/core-commands.ts
+++ src/app/command-parsers/core-commands.ts
@@ -35,13 +35,29 @@ coreCommands.registerCommand({
     const commands = registry.getCommands();
     const registrationData = commands.map(x => registry.getRegistrationData(x));
     const aliases = registrationData.flatMap(x => x.commands.slice(1));
-    const uniqueCommands = commands.filter(x => !aliases.includes(x));
-    const filterOutNoOp = uniqueCommands.filter(x => x !== NO_OP_COMMAND);
-    const output = filterOutNoOp;
-    data.terminal.printAsMachine( output );
+    const uniqueCommands = commands.filter(x => !aliases.includes(x) && x !== NO_OP_COMMAND);
+    const uniqueRegistrationData = uniqueCommands.map(x => registry.getRegistrationData(x));
+
+    const commandsWithShortDescription = uniqueRegistrationData.map(x => {
+      const primaryCommand = x.commands[0];
+      if (x.shortDescription) {
+        const indent = buildIndent(primaryCommand, 10);
+        return `${primaryCommand}${indent}${x.shortDescription}`;
+      } else {
+        return primaryCommand;
+      }
+    });
+
+    data.terminal.printAsMachine( commandsWithShortDescription );
   },
   shortDescription: 'List all available commands',
 });
+
+function buildIndent(command: string, indentBase: number): string {
+  const len = command.length;
+  const indentSize = (Math.ceil(len/indentBase) * indentBase) - len;
+  return ' '.repeat(indentSize);
+}
 
 
 coreCommands.registerCommand({
